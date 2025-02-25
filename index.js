2622e67decc5fed3e9ef2175a28b9db25f403388
swiz9 <swizanu3@gmail.com>
2025-02-25 16:11:43 +0530

    The backend is complete.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.use(express.json());
+
+//routes
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/crud_api")
+  .then(() => {
+    console.log("Connected to Database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
